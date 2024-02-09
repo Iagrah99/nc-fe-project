ae1558587ed688c6d6da5d66f80e3ad04cf560aa
Ian Graham <i4ng1999@gmail.com>
2024-02-09 16:14:52 +0000

    Change img rel link

--- src/Components/TopicCard.jsx
+++ src/Components/TopicCard.jsx
@@ -12,7 +12,7 @@ const TopicCard = ({topic}) => {
   return (
     <Col xl="4" md="6" xs="12">
     <Card style={{ width: '100%', height: "30rem"}}>
-    <Card.Img src={`src/${topic.slug}.jpg`} style={{minHeight: "20rem", maxHeight: "20rem"}} />
+    <Card.Img src={`../src/${topic.slug}.jpg`} style={{minHeight: "20rem", maxHeight: "20rem"}} />
     <Card.Body >
     <Card.Title style={{textAlign: "center"}}>{topic.slug}</Card.Title>
     <Card.Text style={{textAlign: "center"}}>{topic.description}</Card.Text>
