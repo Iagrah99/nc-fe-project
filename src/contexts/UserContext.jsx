65e057da1aae5a4fb7cb9b8d2a79a1fcdb6a4494
Iagrah99 <i4ng1999@gmail.com>
2025-04-26 13:33:34 +0100

    initialise loggedInUser with localStorage or default values

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -4,11 +4,20 @@ import { useState } from "react"
 export const UserContext = createContext()
 
 export const UserProvider = ({children}) => {
-  const [loggedInUser, setLoggedInUser] = useState({
-    username:	 JSON.parse(localStorage.getItem("currentUser")).username,
-    name:	 JSON.parse(localStorage.getItem("currentUser")).name,
-    avatar_url:	 JSON.parse(localStorage.getItem("currentUser")).avatar_url
-  })
+  const [loggedInUser, setLoggedInUser] = useState(() => {
+    const storedUser = JSON.parse(localStorage.getItem("currentUser"));
+  
+    if (storedUser) {
+      return storedUser;
+    } else {
+      return {
+        username: "tickle122",
+        name: "Tom Tickle",
+        avatar_url: "https://res.cloudinary.com/dafsdsmus/image/upload/v1745597600/tomtickle_l7hyac_gq6mrf.png"
+      };
+    }
+  });
+  
 
   return (
     <UserContext.Provider value={{ loggedInUser, setLoggedInUser }}>
