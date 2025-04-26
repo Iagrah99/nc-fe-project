25aa817b7a04a8a5ae969d27dc0cb78db6e2b05f
Iagrah99 <i4ng1999@gmail.com>
2025-04-26 12:47:11 +0100

    Update comments loading styles

--- src/Components/CommentsLoading.jsx
+++ src/Components/CommentsLoading.jsx
@@ -1,15 +1,14 @@
-import { Container } from 'react-bootstrap';
-import textStyles from "../css/TextCSSModule.module.css"
-import Spinner from 'react-bootstrap/Spinner';
 const CommentsLoading = () => {
-  return ( 
-    <Container fluid="xl">
-        <div className={textStyles.center} style={{ marginBlock: '15px' }}>
-        <h2>Loading Comments...</h2>
-        <Spinner animation="border" role="status"/>
-        </div>
-      </Container>
-   );
+  return (
+    <div className="flex flex-col items-center justify-evenly px-4">
+      <div className="flex flex-col items-center justify-center gap-5">
+        <h1 className="text-2xl text-white text-center">
+          Loading Comments
+        </h1>
+        <div className="w-12 h-12 border-4 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
+      </div>
+    </div>
+  );
 }
  
 export default CommentsLoading;
